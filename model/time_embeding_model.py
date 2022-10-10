83f2858c10f9a61d0aee360cddc479cc206add8f
veya2ztn <zhangtianning110@gmail.com>
2022-10-11 00:11:21 +0800

    add timestamp embedding model; then modify the once_forward function in train.py

---
+++ model/time_embeding_model.py
@@ -0,0 +1,57 @@
+import torch,time
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from .afnonet import BaseModel
+
+class Time_Sphere_Model(BaseModel):
+    '''
+    x_data is the Field data without time information as (B, T, h ,w,  P)
+    x_timestamp is the stamp vector via time_features
+     it is a (B, T, 2) data, represent which day in year, which hour in the day,  
+    the feature will be projected into a sphere coordinate by use the 
+    [Pcos(year_pos),Psin(year_pos)cos(day_pos),Psin(year_pos)cos(day_pos)]
+    so the final tensor is (B, T, h ,w,  P, 3) --> (B, T, h ,w,  3P)
+    ------------------------------------------------------------------------
+    after passing this tensor into the backbone 
+    (B, T, h ,w, 3P) --> backbone --> (B, T, h ,w, 3P) --> (B, T, h ,w, P,3)
+    ------------------------------------------------------------------------
+    we will do the norm operation at end, and 
+    norm( [B, T, h ,w, P,3] ) -->  [B, T, h ,w, P] 
+    ---------------------------------------------------------------------------
+    if the y_timestamp is given, then extraloss to reforce the sphere constrain.
+      cos([out1,out2,out3], [cos(year_pos),sin(year_pos)cos(day_pos),sin(year_pos)cos(day_pos)])must equal 1
+    '''
+    def __init__(self, args, backbone):
+        super().__init__()
+        self.backbone =  backbone
+        self.block_target_timestamp = args.block_target_timestamp
+        self.timespace_shape=(args.history_length,*img_size) if args.history_length>1 else img_size
+    def forward(self, x, x_timestamp, y_timestamp):
+        # assume input is (B, P ,T, h ,w)
+        assert x.shape[2:] == self.timespace_shape
+        x_timestamp = x_timestamp.squeeze(1)
+        y_timestamp = y_timestamp.squeeze(1)
+        if x_timestamp.shape[-1]==4:
+            x_timestamp = x_timestamp[...,[0,-1]] # if the time feature is 4 then only the first and last is needed
+            y_timestamp = y_timestamp[...,[0,-1]]
+        B, P = x.shape[:2]
+        year_pos_x = x_timestamp[...,0]
+        day_pos_x  = x_timestamp[...,1]
+        _input = torch.stack([x*torch.cos(year_pos_x),
+                    x*torch.sin(year_pos_x)*torch.cos(day_pos_x),
+                    x*torch.cos(year_pos_x)*torch.sin(day_pos_x)],1) # (B, 3P ,T, h ,w)
+        x = self.backbone(x)
+        x = x.reshape(B,3,P,*self.timespace_shape)
+        extra_loss= 0
+        if self.block_target_timestamp:
+            year_pos_y = y_timestamp[...,0]
+            day_pos_y = y_timestamp[...,1]
+            output_direction = torch.Tensor([torch.cos(year_pos_y),
+                            torch.sin(year_pos_y)*torch.cos(day_pos_y),
+                            torch.cos(year_pos_y)*torch.sin(day_pos_y)])
+            output_direction_shape=[B,3]+[1]*len(self.timespace_shape)        
+            output_direction = output_direction.reshape(*output_direction_shape)
+            extra_loss = torch.nn.functional.cosine_similarity(x,n,dim=1).mean()
+        x = x.norm(dim=1)
+        return x, extra_loss
--- optuna_run.sh
+++ optuna_run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#SBATCH -J F7066A      # 作业在调度系统中的作业名为myFirstJob;
-#SBATCH -o log/%j-F7066A.out  # 脚本执行的输出将被保存在20210827-%j.out文件下，%j表示作业号;
-#SBATCH -e log/%j-F7066A.out  # 脚本执行的输出将被保存在20210827-%j.out文件下，%j表示作业号;
+#SBATCH -J WB70JCA      # 作业在调度系统中的作业名为myFirstJob;
+#SBATCH -o log/%j-WB70JCA.out  # 脚本执行的输出将被保存在20210827-%j.out文件下，%j表示作业号;
+#SBATCH -e log/%j-WB70JCA.out  # 脚本执行的输出将被保存在20210827-%j.out文件下，%j表示作业号;
 
 # python optuna_run.py --train_set 4796ad --epoch 60 --mode pretrain --time_step 2 --patch_size 1,3,3 \
 #                      --img_size 3,51,96 --dataset_type ERA5Tiny12_47_96 --input_channel 4 --output_channel 4 \
@@ -21,11 +21,15 @@
 # --model_type FEDformer --embed_dim 256 --use_amp 0 --batch_size 16 --model_depth 1 --valid_batch_size 32 \
 # --clip-grad 1e4
 
-#python optuna_run.py --train_set 2D706N --dataset_type WeathBench7066 --time_step 3 --epoch 100 --mode pretrain --dataset_flag 3D70U --save_warm_up 1000 --wrapper_model ConVectionModel --debug 0 --use_amp 0
+
+### AFNONetJC
+#python optuna_run.py --train_set 2D706N --dataset_type WeathBench7066 --model_type AFNONetJC --time_step 2 --epoch 100 --mode pretrain --dataset_flag 3D70U --save_warm_up 1000 --wrapper_model ConVectionModel --debug 0 --use_amp 0
+python optuna_run.py --train_set 2D706N --dataset_type WeathBench7066 --model_type AFNONetJC --time_step 2 --epoch 100 --mode pretrain --save_warm_up 1000 --debug 0 --use_amp 0
+
 # python optuna_run.py --train_set 2D706N --dataset_type WeathBench7066 --time_step 3 --epoch 100 --mode finetune \
 # --dataset_flag 3D70U --save_warm_up 1000 --wrapper_model ConVectionModel --use_amp 0 \
 # --pretrain_weight checkpoints/WeathBench7066/AFNONet/time_step_2_pretrain-2D706N_every_1_step/10_07_00_19_40-seed_2641/backbone.best.pt 
 
 #python optuna_run.py --train_set 2D706N --dataset_type WeathBench7066 --time_step 3 --epoch 100 --mode pretrain --save_warm_up 1000
-python optuna_run.py --train_set 2D706N --dataset_type WeathBench7066 --time_step 3 --epoch 100 --mode finetune --save_warm_up 1000 \
---pretrain_weight checkpoints/WeathBench7066/AFNONet/time_step_2_pretrain-2D706N_every_1_step/10_08_21_16_55-seed_28356/backbone.best.pt
+# python optuna_run.py --train_set 2D706N --dataset_type WeathBench7066 --time_step 3 --epoch 100 --mode finetune --save_warm_up 1000 \
+# --pretrain_weight checkpoints/WeathBench7066/AFNONet/time_step_2_pretrain-2D706N_every_1_step/10_08_21_16_55-seed_28356/backbone.best.pt
