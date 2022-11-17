c0e180e488a7eec03d84d6f51bc95a768266d0d7
veya2ztn <zhangtianning110@gmail.com>
2022-11-17 15:04:39 +0800

    will put some jupyter test code into archieve file

--- utils/tools.py
+++ utils/tools.py
@@ -94,3 +94,42 @@ def get_center_around_indexes(patch_range,img_shape):
     indexes = indexes.reshape(len(wlist),len(hlist),2,patch_range,patch_range).transpose(1,0,2,3,4)
     coor    = coor.reshape(len(wlist),len(hlist),2).transpose(2,1,0)
     return coor, indexes
+
+
+if __name__ == '__main__':
+    img_shape = (32,64)
+    patch_range = 5
+    test_tensor = np.arange(32*64).reshape(32,64)
+    coor, indexes = get_center_around_indexes(patch_range,img_shape)
+    center_value_array_from_index = test_tensor[coor[0],coor[1]]
+    pos_x   = 2
+    pos_y   = 4
+    center_x,center_y = coor[:,pos_x,pos_y]
+    center_value      = test_tensor[center_x,center_y]
+    around_x,around_y = indexes[pos_x,pos_y]
+    around            = test_tensor[around_x,around_y]
+    center_value_from_index = center_value_array_from_index[2,4]
+    print(f'''
+    the goal of `get_center_around_indexes` and `get_patch_location_index` is to get a 
+    - coor[center_tensor] = (2, 28, 64) # if we use (32, 64) earth grid as example,
+        - each slot in (28,64) is an pos size like (2,). for example [1,2]
+    - indexes[around_tensor] = (28, 64, 2, 5, 5)
+        - each slot in (28,64) is an pos array like (2,5,5). 
+            the first (1,5,5) is all the x-index and next (1,5,5) is all the y-index
+    Here is 28 rather than 32 is because we temperal dont deal with the boundary condition in lattitude.
+    And the longitude is obviously peroid.
+    for example, if we have a naive increase matrix like 
+    ''')
+    print(test_tensor)
+    print("then the cneter_tensor get by `test_tensor[coor[0],coor[1]]` ")
+    print(center_value_array_from_index)
+    print(f"if we pick i={pos_x},y={pos_y} block here, then the center is ")
+
+    print(center_value)
+    print('and the center value can get from `center_x,center_y = coor[:,pos_x,pos_y];center_value      = test_tensor[center_x,center_y]`')
+    print(around)
+    print("its is obviously got from the subarray by center_value_array_from_index[2,4]")
+    print(center_value_from_index)
+
+
+    
