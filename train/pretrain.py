66c6f53eaadd76d1e96f6911402d963cc0efeeff
zhangtianning <zhangtianning110@outlook.com>
2023-01-17 18:51:31 +0800

    fix loss wall bug. it may pollute some experiment

--- train/pretrain.py
+++ train/pretrain.py
@@ -809,7 +809,7 @@ def run_one_epoch_normal(epoch, start_step, model, criterion, data_loader, optim
                     with torch.cuda.amp.autocast(enabled=model.use_amp):
                         rotation_loss= grad_modifier.getRotationDeltaloss(model.module if hasattr(model,'module') else model, #<-- its ok use `model.module`` or `model`, but model.module avoid unknow error of functorch 
                                 batch[0], ltmv_pred.detach() ,target,rotation_regular_mode = grad_modifier.rotation_regular_mode)                    
-                    if grad_modifier.loss_wall and rotation_loss > grad_modifier.loss_wall: 
+                    if rotation_loss > grad_modifier.loss_wall: 
                         the_loss = rotation_loss*grad_modifier.gd_alpha
                         loss_scaler.scale(the_loss).backward() 
                     # if grad_modifier.use_amp:
