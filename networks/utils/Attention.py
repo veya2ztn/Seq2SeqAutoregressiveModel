60e2d6039d82a7dab8566e99d4cb6572a504fe2e
zhangtianning <zhangtianning110@gmail.com>
2023-02-27 19:21:52 +0800

    Add Cross LgNet

--- networks/utils/Attention.py
+++ networks/utils/Attention.py
@@ -668,6 +668,232 @@ class SD_attn(nn.Module):
         return x
 
 
+class SD_attn_Cross(nn.Module):
+    def __init__(self, dim, window_size, num_heads, qkv_bias=True, attn_drop=0., proj_drop=0., shift_size=[0, 0, 0], dilated_size=[1,1,1]) -> None:
+        super().__init__()
+        self.dim = dim
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = head_dim ** -0.5
+        
+        self.dilated_size = dilated_size[-len(window_size):]
+        self.window_size = window_size
+        self.shift_size = shift_size
+        self.total_window_size = [window_size[i] * dilated_size[i] for i in range(len(window_size))]
+        
+
+
+        if len(self.window_size) == 2:
+            self.rope_quad = rope2(self.window_size, head_dim)
+        elif len(self.window_size) == 3:
+            self.rope_quad = rope3(self.window_size, head_dim)
+       
+        self.qv = nn.Linear(dim, dim * 2, bias=qkv_bias)
+        self.k  = nn.Linear(dim, dim * 1, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+        self.softmax = nn.Softmax(dim=-1)
+
+        if len(window_size) == 2:
+            self.position_enc = rope2(window_size, head_dim)
+        elif len(window_size) == 3:
+            self.position_enc = rope3(window_size, head_dim)
+    
+    def create_mask(self, x):
+            # calculate attention mask for SW-MSA
+            # 保证Hp和Wp是window_size的整数倍
+            # Hp = int(np.ceil(H / self.window_size[0])) * self.window_size[0]
+            # Wp = int(np.ceil(W / self.window_size[1])) * self.window_size[1]
+            # 拥有和feature map一样的通道排列顺序，方便后续window_partition
+
+        if len(self.window_size) == 3:
+            _, T, H, W, _ = x.shape
+            img_mask = torch.zeros(
+                (1, T, H, W, 1), device=x.device)  # [1, Hp, Wp, 1]
+            t_slices = (slice(0, -self.window_size[0]),
+                        slice(-self.window_size[0], -self.shift_size[0]),
+                        slice(-self.shift_size[0], None))
+            h_slices = (slice(0, -self.window_size[1]),
+                        slice(-self.window_size[1], -self.shift_size[1]),
+                        slice(-self.shift_size[1], None))
+            w_slices = (slice(0, -self.window_size[2]),
+                        slice(-self.window_size[2], 0),
+                        slice(0, None))
+            cnt = 0
+            for t in t_slices:
+                for h in h_slices:
+                    for w in w_slices:
+                        img_mask[:, t, h, w, :] = cnt
+                        cnt += 1
+        elif len(self.window_size) == 2:
+            _, H, W, _ = x.shape
+            img_mask = torch.zeros(
+                (1, H, W, 1), device=x.device)  # [1, Hp, Wp, 1]
+            h_slices = (slice(0, -self.window_size[0]),
+                        slice(-self.window_size[0], -self.shift_size[0]),
+                        slice(-self.shift_size[0], None))
+            w_slices = (slice(0, -self.window_size[1]),
+                        slice(-self.window_size[1], 0),
+                        slice(0, None))
+            cnt = 0
+            for h in h_slices:
+                for w in w_slices:
+                    img_mask[:, h, w, :] = cnt
+                    cnt += 1
+
+        mask_windows = window_partition(
+            img_mask, self.total_window_size)  # [B, nW, Mt, Mh, Mw, C]
+        mask_windows = mask_windows.reshape(-1, *self.total_window_size, 1)
+        B_ = mask_windows.shape[0]
+        if len(self.dilated_size) == 3:
+            mask_windows = window_partition(mask_windows, self.dilated_size).reshape(B_, -1,
+                                                                                     self.dilated_size[0]*self.dilated_size[1]*self.dilated_size[2], 1).permute(
+                0, 2, 1, 3).reshape(B_*self.dilated_size[0]*self.dilated_size[1]*self.dilated_size[2], -1)
+        elif len(self.dilated_size) == 2:
+            mask_windows = window_partition(mask_windows, self.dilated_size).reshape(B_, -1,
+                                                                                     self.dilated_size[0]*self.dilated_size[1], 1).permute(
+                0, 2, 1, 3).reshape(B_*self.dilated_size[0]*self.dilated_size[1], -1)
+
+        # mask_windows = window_partition(img_mask, self.window_size)  # [nW, Mh, Mw, 1]
+        # if len(self.window_size) == 3:
+        #     mask_windows = mask_windows.view(-1, self.window_size[0] * self.window_size[1] * self.window_size[2])  # [nW, Mh*Mw]
+        # elif len(self.window_size) == 2:
+        #     mask_windows = mask_windows.view(-1, self.window_size[0] * self.window_size[1])  # [nW, Mh*Mw]
+
+        # [nW, 1, Mh*Mw] - [nW, Mh*Mw, 1]
+        attn_mask = mask_windows.unsqueeze(1) - mask_windows.unsqueeze(2)
+        # [nW, Mh*Mw, Mh*Mw]
+        attn_mask = attn_mask.masked_fill(
+            attn_mask != 0, -torch.inf).masked_fill(attn_mask == 0, float(0.0))
+        return attn_mask
+
+    def create_windows(self,x):
+        if len(self.window_size) == 2:
+            _, H, W, C = x.shape
+        elif len(self.window_size) == 3:
+            _, T, H, W, C = x.shape
+
+        
+
+        if self.shift_size[-1] > 0:
+            if len(self.window_size) == 3:
+                shifted_x = torch.roll(
+                    x, shifts=(-self.shift_size[0], -self.shift_size[1], -self.shift_size[2]), dims=(1, 2, 3))
+            elif len(self.window_size) == 2:
+                shifted_x = torch.roll(
+                    x, shifts=(-self.shift_size[0], -self.shift_size[1]), dims=(1, 2))
+        else:
+            shifted_x = x
+            mask = None
+
+        # qkv(): -> [2*batch_size*num_windows, Mh*Mw, 3 * total_embed_dim]
+        # reshape: -> [2*batch_size*num_windows, Mh*Mw, 3, num_heads, embed_dim_per_head]
+        # permute: -> [3, 2*batch_size*num_windows, num_heads, Mh*Mw, embed_dim_per_head]
+
+        x_windows = window_partition(
+            shifted_x, self.total_window_size)  # [B, nW, Mt, Mh, Mw, C]
+        x_windows = x_windows.reshape(-1, *self.total_window_size, C)
+        B = x_windows.shape[0]
+
+        dodilate = False
+        if len(self.dilated_size) == 3:
+            x_windows = window_partition(x_windows, self.dilated_size).reshape(B, -1, self.dilated_size[0]*self.dilated_size[1]*self.dilated_size[2], C
+                                                                               ).permute(0, 2, 1, 3).reshape(B*self.dilated_size[0]*self.dilated_size[1]*self.dilated_size[2], -1, C)
+            dodilate = True
+        elif len(self.dilated_size) == 2:
+            x_windows = window_partition(x_windows, self.dilated_size).reshape(B, -1, self.dilated_size[0]*self.dilated_size[1], C
+                                                                               ).permute(0, 2, 1, 3).reshape(B*self.dilated_size[0]*self.dilated_size[1], -1, C)
+            dodilate = True
+        return x_windows
+    
+    def forward(self, x, cross):
+        """
+        Args:
+            x: input features with shape of (num_windows*B, Mh*Mw, C)
+            cross: input features with shape of (num_windows*B, Mh*Mw, C)
+            mask: (0/-inf) mask with shape of (num_windows, Wh*Ww, Wh*Ww) or None
+        """
+        # [batch_size, Mt, Mh, Mw, total_embed_dim]
+
+        T = 1
+        B = x.size(0)
+        if len(self.window_size) == 2:
+            _, H, W, C = x.shape
+        elif len(self.window_size) == 3:
+            _, T, H, W, C = x.shape
+        if (self.shift_size[-1] == 0) or (self.total_window_size[-1] == W):
+            mask = None
+        else:
+            mask = self.create_mask(x)
+        c_windows = self.create_windows(cross)
+        x_windows = self.create_windows(x)
+
+        assert len(c_windows) == len(x_windows)
+        
+        B_, N, C = x_windows.shape
+        qv = self.qv(x_windows).reshape(B_, N, 2, self.num_heads,C // self.num_heads).permute(2, 0, 3, 1, 4)
+        # [batch_size*num_windows, num_heads, Mh*Mw, embed_dim_per_head]
+        # make torchscript happy (cannot use tensor as tuple)
+        q, v = qv.unbind(0)
+        k    = self.k(c_windows).reshape(B_, N, 1, self.num_heads,C // self.num_heads).permute(2, 0, 3, 1, 4)
+
+        q = self.position_enc(q.reshape(-1, *self.window_size, C // self.num_heads)).reshape(B_, self.num_heads, -1, C // self.num_heads)
+        k = self.position_enc(k.reshape(-1, *self.window_size, C // self.num_heads)).reshape(B_, self.num_heads, -1, C // self.num_heads)
+
+        # transpose: -> [batch_size*num_windows, num_heads, embed_dim_per_head, Mh*Mw]
+        # @: multiply -> [batch_size*num_windows, num_heads, Mh*Mw, Mh*Mw]
+        q = q * self.scale
+        attn = (q @ k.transpose(-2, -1))
+
+        if mask is not None:
+            # mask: [nW, Mh*Mw, Mh*Mw]
+            nW = mask.shape[0]  # num_windows
+            # attn.view: [batch_size, num_windows, num_heads, Mh*Mw, Mh*Mw]
+            # mask.unsqueeze: [1, nW, 1, Mh*Mw, Mh*Mw]
+            attn = attn.view(B_ // nW, nW, self.num_heads, N,
+                             N) + mask.unsqueeze(1).unsqueeze(0)
+            attn = attn.view(-1, self.num_heads, N, N)
+            attn = self.softmax(attn)
+        else:
+            attn = self.softmax(attn)
+
+        attn = self.attn_drop(attn)
+
+        # @: multiply -> [batch_size*num_windows, num_heads, Mh*Mw, embed_dim_per_head]
+        # transpose: -> [batch_size*num_windows, Mh*Mw, num_heads, embed_dim_per_head]
+        # reshape: -> [batch_size*num_windows, Mh*Mw, total_embed_dim]
+        attn_windows = (attn @ v).transpose(1, 2).reshape(B_, N, C)
+
+        if len(self.window_size) == 3:
+            attn_windows = attn_windows.reshape(B, -1, N, C).permute(0, 2, 1, 3).reshape(
+                -1, self.dilated_size[0]*self.dilated_size[1]*self.dilated_size[2], C)
+            attn_windows = window_reverse(
+                attn_windows, self.dilated_size, *self.total_window_size)
+        elif len(self.window_size) == 2:
+            attn_windows = attn_windows.reshape(B, -1, N, C).permute(0, 2, 1, 3).reshape(
+                -1, self.dilated_size[0]*self.dilated_size[1], C)
+            attn_windows = window_reverse(
+                attn_windows, self.dilated_size, 1, *self.total_window_size)
+
+        shifted_x = window_reverse(
+            attn_windows, self.total_window_size, T, H, W)
+
+        if self.shift_size[0] > 0:
+            if len(self.window_size) == 3:
+                x = torch.roll(shifted_x, shifts=(
+                    self.shift_size[0], self.shift_size[1], self.shift_size[2]), dims=(1, 2, 3))
+            elif len(self.window_size) == 2:
+                x = torch.roll(shifted_x, shifts=(
+                    self.shift_size[0], self.shift_size[1]), dims=(1, 2))
+        else:
+            x = shifted_x
+
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
 
 
 class Flash_attn(nn.Module):
--- networks/utils/Blocks.py
+++ networks/utils/Blocks.py
@@ -2,7 +2,7 @@ from turtle import forward
 import torch
 import torch.nn as nn
 from networks.utils.utils import DropPath, PeriodicPad2d, Mlp
-from networks.utils.Attention import SD_attn, WindowAttention, HiLo, Conv_attn, SD_attn_parallel
+from networks.utils.Attention import SD_attn, WindowAttention, HiLo, Conv_attn, SD_attn_parallel, SD_attn_Cross
 from networks.utils.mlp import DWMlp
 from networks.utils.Attention import SD_attn_withmoe
 from networks.utils.mlp import Mlp_withmoe, Mlp_parallel
@@ -104,6 +104,64 @@ class Windowattn_block(nn.Module):
 
 
         return x
+
+
+class Windowattn_Cross_block(nn.Module):
+    def __init__(self, dim, window_size, num_heads=1, mlp_ratio=4.,
+                 qkv_bias=True, drop=0., attn_drop=0., drop_path=0.,
+                 act_layer=nn.GELU, norm_layer=nn.LayerNorm,
+                 attn_type="windowattn", pre_norm=True, **kwargs):
+        super().__init__()
+        self.dim = dim
+        self.window_size = window_size
+        self.mlp_ratio = mlp_ratio
+        self.pre_norm = pre_norm
+
+        self.norm = norm_layer(dim)
+        # self.GAU1 = Flash_attn(dim, window_size=self.window_size, uv_bias=qkv_bias, attn_drop=attn_drop, proj_drop=drop, expansion_factor=2, attn_type='lin')
+        if attn_type == "windowattn":
+            if "shift_size" not in kwargs:
+                shift_size = [0, 0, 0]
+            else:
+                shift_size = kwargs["shift_size"]
+            if "dilated_size" in kwargs:
+                dilated_size = kwargs["dilated_size"]
+            else:
+                dilated_size = [1, 1, 1]
+            self.attn = SD_attn_Cross(
+                dim, window_size=self.window_size, num_heads=num_heads, qkv_bias=qkv_bias,
+                attn_drop=attn_drop, proj_drop=drop, shift_size=shift_size, dilated_size=dilated_size)
+        elif attn_type == "convattn":
+            self.attn = Conv_attn(
+                dim, window_size, num_heads, qkv_bias=qkv_bias, attn_drop=attn_drop, proj_drop=drop)
+
+        self.drop_path = DropPath(
+            drop_path) if drop_path > 0. else nn.Identity()
+
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim,
+                       act_layer=act_layer, drop=drop)
+
+    def forward(self, x, cross):
+        shortcut = x
+        # partition windows
+
+        if self.pre_norm:
+            x = shortcut + self.drop_path(self.attn(self.norm(x), (self.norm(cross))))
+        else:
+            x = self.norm(shortcut + self.drop_path(self.attn(x, cross)))
+
+        # W-MSA/SW-MS
+
+        if self.pre_norm:
+            x = x + self.drop_path(self.mlp(self.norm2(x)))
+        else:
+            x = self.norm2(x + self.drop_path(self.mlp(x)))
+
+        return x
+
+
 
 class Hilo_Block(nn.Module):
     def __init__(self, dim, window_size, num_heads=1, mlp_ratio=4., 
