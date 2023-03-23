9a2efd4efaff65d39a9f40caf53d9717412da698
veya2ztn <zhangtianning110@outlook.com>
2023-03-23 11:12:57 +0800

    lets tune the dropout to make grow up work

--- model/othermodels.py
+++ model/othermodels.py
@@ -173,7 +173,8 @@ class MLP2GrowUp_CK_LgNet(CK_LgNet):
 
     def set_step(self, step=None, epoch=None, step_total=None, eval_mode=False):
         if epoch == 0:
-            p = 0.3*np.exp(- step/step_total*100)
+            step_total = 100
+            p = 0.3*np.exp(- step/step_total) if step<step_total else 0
             set_dropout(self.backbone,p)
 from networks.LGCrossNet import LGNetCross
 class CK_LgNet_Cross(BaseModel):
